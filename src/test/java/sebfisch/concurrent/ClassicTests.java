72c2de4fb8c9a95ec804013e5e21307204dfeae2
Sebastian Fischer <mail@sebfisch.de>
2021-01-13 01:36:15 +0100

    renamed classic tests

--- src/test/java/sebfisch/concurrent/ClassicTests.java
+++ src/test/java/sebfisch/concurrent/ClassicTests.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 
-public class ThreadTests {
+public class ClassicTests {
 
     @Test
     void testHowToStartAndJoinThreads() {
