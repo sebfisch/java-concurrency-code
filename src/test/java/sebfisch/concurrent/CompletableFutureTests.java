c1583c0a02c31c2e809a29da2fff475a046a35da
Sebastian Fischer <mail@sebfisch.de>
2021-01-13 07:53:10 +0100

    add completable future tests

---
+++ src/test/java/sebfisch/concurrent/CompletableFutureTests.java
@@ -0,0 +1,49 @@
+package sebfisch.concurrent;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.Test;
+
+public class CompletableFutureTests {
+
+    @Test
+    void testCreatingCompletedFuture() {
+        final CompletableFuture<Integer> future = //
+            CompletableFuture.completedFuture(42);
+        assertEquals(42, future.join());
+    }
+
+    @Test
+    void testCompletingFutureAsynchronously() {
+        final CompletableFuture<Integer> future = //
+            CompletableFuture.supplyAsync(() -> 42);
+        assertEquals(42, future.join());
+    } 
+    
+    @Test
+    void testCompletingFutureManually() {
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        Executors.newSingleThreadExecutor().execute(() -> future.complete(42));
+        assertEquals(42, future.join());
+    }
+
+    @Test
+    void testThatThenApplyIsLikeMap() {
+        final CompletableFuture<String> stringFuture = //
+            CompletableFuture.completedFuture("hello");
+        final CompletableFuture<Integer> intFuture = //
+            stringFuture.thenApply(String::length);
+        assertEquals(5, intFuture.join());
+    }
+
+    @Test
+    void testThatThenComposeIsLikeFlatMap() {
+        final CompletableFuture<String> stringFuture = //
+            CompletableFuture.completedFuture("ha");
+        final CompletableFuture<String> combinedFuture = //
+            stringFuture.thenCompose(string -> //
+            CompletableFuture.completedFuture(string+string));
+        assertEquals("haha", combinedFuture.join());
+    }
+}
