eeb61ea7e096511a5a831a700868a5723915ed4a
Sebastian Fischer <mail@sebfisch.de>
2021-01-13 03:48:13 +0100

    add classic lock tests

--- src/test/java/sebfisch/concurrent/ClassicTests.java
+++ src/test/java/sebfisch/concurrent/ClassicTests.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 
 public class ClassicTests {
@@ -31,7 +34,8 @@ public class ClassicTests {
         try {
             one.join();
             two.join();
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+        }
 
         assertFalse(one.isAlive());
         assertFalse(two.isAlive());
@@ -53,11 +57,135 @@ public class ClassicTests {
 
         thread.start();
         thread.interrupt();
-        
+
         try {
             thread.join();
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+        }
 
         assertFalse(thread.isInterrupted());
+    }
+
+    private static class Count {
+        public int value = 0;
+    }
+
+    private void randomSleep() {
+        try {
+            TimeUnit.MILLISECONDS.sleep( //
+                    ThreadLocalRandom.current().nextInt(10) //
+            );
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Test
+    void testThreadInterference() {
+        final Count count = new Count();
+
+        final Runnable inc = () -> {
+            final int val = count.value;
+            randomSleep();
+            count.value = val + 1;
+        };
+
+        final Runnable dec = () -> {
+            final int val = count.value;
+            randomSleep();
+            count.value = val - 1;
+        };
+
+        final IntSupplier race = () -> {
+            final Thread incThread = new Thread(inc);
+            final Thread decThread = new Thread(dec);
+            incThread.start();
+            decThread.start();
+            try {
+                incThread.join();
+                decThread.join();
+            } catch (InterruptedException e) {
+            }
+            return count.value;
+        };
+
+        assertTrue(IntStream.generate(race).limit(100).anyMatch(n -> n != 0));
+    }
+
+    @Test
+    void testIntrinsicLock() {
+        final Count count = new Count();
+
+        final Runnable inc = () -> {
+            synchronized (count) {
+                final int val = count.value;
+                randomSleep();
+                count.value = val + 1;
+            }
+        };
+
+        final Runnable dec = () -> {
+            synchronized (count) {
+                final int val = count.value;
+                randomSleep();
+                count.value = val - 1;
+            }
+        };
+
+        final IntSupplier race = () -> {
+            final Thread incThread = new Thread(inc);
+            final Thread decThread = new Thread(dec);
+            incThread.start();
+            decThread.start();
+            try {
+                incThread.join();
+                decThread.join();
+            } catch (InterruptedException e) {}
+            return count.value;
+        };
+
+        assertTrue(IntStream.generate(race).limit(100).allMatch(n -> n == 0));
+    }
+
+    @Test
+    void testIntrinsicLockRelease() {
+        final Count count = new Count();
+
+        final Runnable consume = () -> {
+            synchronized (count) {
+                while (count.value == 0) {
+                    try {
+                        count.wait();
+                    } catch (InterruptedException e) {}
+                }
+                count.value = 0;
+                count.notifyAll();
+            }
+        };
+
+        final Runnable produce = () -> {
+            synchronized (count) {
+                while (count.value != 0) {
+                    try {
+                        count.wait();
+                    } catch (InterruptedException e) {}
+                }
+                count.value = 10;
+                count.notifyAll();
+            }
+        };
+
+        final IntSupplier handshake = () -> {
+            final Thread consumer = new Thread(consume);
+            final Thread producer = new Thread(produce);
+            consumer.start();
+            producer.start();
+            try {
+                consumer.join();
+                producer.join();
+            } catch (InterruptedException e) {}
+            return count.value;
+        };
+
+        assertTrue(IntStream.generate(handshake).limit(100).allMatch(n -> n == 0));
     }
 }
